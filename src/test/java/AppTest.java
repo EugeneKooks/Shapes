3839e957b63897ffb3b746d913d4b297ac27c31d
EugeneKooks <eugene.kooks@gmail.com>
2017-06-05 00:30:26 +0300

    Some changes in tests

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -10,11 +10,6 @@ public class AppTest {
         File myFile = new File("src\\main\\java\\by\\kooks\\files\\source_file.txt");
         ArrayList<Double> allCoordinates = CoordinatesReader.readFromFile(myFile);
         ArrayList<Triangle> allTriangles = TriangleCreator.createTrianglesFromPoints(allCoordinates);
-        Triangle triangle0 = allTriangles.get(0);
-        triangle0.setP1(new Point(allCoordinates.get(6), allCoordinates.get(7)));
-        triangle0.setP2(new Point(allCoordinates.get(8), allCoordinates.get(9)));
-        triangle0.setP3(new Point(allCoordinates.get(10), allCoordinates.get(11)));
-        System.out.println(triangle0.equals(allTriangles.get(1)));
         double square = TriangleProcessing.square(allTriangles.get(0));
         double perimeter = TriangleProcessing.perimeter(allTriangles.get(0));
         TriangleParameters parameters0 = new TriangleParameters(square,perimeter);
